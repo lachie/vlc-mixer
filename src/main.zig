b54252119058641afd8225bc03bf615622052742
Lachie Cox <lachiec@gmail.com>
2021-07-01 14:31:44 +1000

    zig fmt

--- src/main.zig
+++ src/main.zig
@@ -34,7 +34,7 @@ pub fn OpenFilterC(p_this: [*c]c.vlc_object_t) callconv(.C) c_int {
 
 fn ConvertC(p_filter: [*c]c.filter_t, in_block: [*c]c.block_t) callconv(.C) [*c]c.block_t {
     const filter: *Filter = @ptrCast(*Filter, @alignCast(std.meta.alignment(*Filter), p_filter.*.p_sys));
-    if(filter.Convert(in_block)) |out_block| {
+    if (filter.Convert(in_block)) |out_block| {
         if (out_block != in_block) {
             filter.blockRelease(in_block);
         }
@@ -78,7 +78,7 @@ const Filter = struct {
 
         filter.output_sample_size = @divTrunc(filter.audio_out.i_bitspersample * filter.audio_out.i_channels, 8);
 
-        if(filter.output_sample_size == 0) {
+        if (filter.output_sample_size == 0) {
             return error.FilterInitFailed;
         }
 
@@ -89,7 +89,7 @@ const Filter = struct {
         return filter;
     }
 
-    fn Convert(self: *Filter, in_block: *c.block_t ) ![*c]c.block_t {
+    fn Convert(self: *Filter, in_block: *c.block_t) ![*c]c.block_t {
         var out_block = try self.outputBlock(in_block);
 
         // const fmt2 = AudioBitFormat(channels{ .left = true, .right = true });
@@ -100,7 +100,7 @@ const Filter = struct {
         var dst = mix2m.castDst(out_block.*.p_buffer);
 
         var i: u32 = 0;
-        while(i < in_block.*.i_nb_samples) : (i += 1) {
+        while (i < in_block.*.i_nb_samples) : (i += 1) {
             // try self.mixer(&src[i], &dst[i]);
             try mix2m.mix(&src[i], &dst[i]);
         }
@@ -120,11 +120,11 @@ const Filter = struct {
     }
 
     fn outputBlock(self: *Filter, in_block: [*c]c.block_t) ![*c]c.block_t {
-        if(in_block == null or in_block.*.i_nb_samples == 0) {
+        if (in_block == null or in_block.*.i_nb_samples == 0) {
             return null;
         }
         const out_size = in_block.*.i_nb_samples * self.output_sample_size;
-        var out_block = try self.blockAlloc( out_size );
+        var out_block = try self.blockAlloc(out_size);
 
         out_block.*.i_nb_samples = in_block.*.i_nb_samples;
         out_block.*.i_dts = in_block.*.i_dts;
@@ -142,7 +142,7 @@ const Filter = struct {
 fn Mixer(comptime SrcT: type, comptime DstT: type) type {
     return struct {
         const Self = @This();
-        mix: fn(s: anytype, d: anytype) anyerror!void,
+        mix: fn (s: anytype, d: anytype) anyerror!void,
 
         fn castSrc(comptime _: Self, src: anytype) [*]const SrcT {
             return @ptrCast([*]const SrcT, @alignCast(std.meta.alignment(SrcT), src));
@@ -153,7 +153,7 @@ fn Mixer(comptime SrcT: type, comptime DstT: type) type {
     };
 }
 
-const mix2m = Mixer(AudioBitFormat(channels{ .left = true, .right = true }), AudioBitFormat(channels{.left=true, .right=true})){ .mix = mix2 };
+const mix2m = Mixer(AudioBitFormat(channels{ .left = true, .right = true }), AudioBitFormat(channels{ .left = true, .right = true })){ .mix = mix2 };
 
 fn mix2(in: anytype, out: anytype) !void {
     out.left = in.left;
@@ -190,12 +190,12 @@ const channels = packed struct {
 
     fn get(self: channels, offset: usize, chData: []const f32) []const f32 {
         const channelCount = self.count();
-        return chData[offset * channelCount..(offset+1) * channelCount];
+        return chData[offset * channelCount .. (offset + 1) * channelCount];
     }
 
     fn set(self: channels, offset: usize, chData: []f32, in: []const f32) void {
         const channelCount = self.count();
-        std.mem.copy(f32, chData[offset * channelCount..(offset+1) * channelCount], in[0..channelCount]);
+        std.mem.copy(f32, chData[offset * channelCount .. (offset + 1) * channelCount], in[0..channelCount]);
         return;
     }
 };
@@ -234,26 +234,24 @@ fn AudioBitFormat(comptime ch: channels) type {
 
     // TODO use @field
     if (ch.centre) {
-        fields[i] =.{ .name = "centre", .field_type = f32, .default_value = default_value, .is_comptime = false, .alignment = alignment }; 
+        fields[i] = .{ .name = "centre", .field_type = f32, .default_value = default_value, .is_comptime = false, .alignment = alignment };
         i += 1;
     }
     if (ch.left) {
-        fields[i] =.{ .name = "left", .field_type = f32, .default_value = default_value, .is_comptime = false, .alignment = alignment }; 
+        fields[i] = .{ .name = "left", .field_type = f32, .default_value = default_value, .is_comptime = false, .alignment = alignment };
         i += 1;
     }
     if (ch.right) {
-        fields[i] =.{ .name = "right", .field_type = f32, .default_value = default_value, .is_comptime = false, .alignment = alignment }; 
+        fields[i] = .{ .name = "right", .field_type = f32, .default_value = default_value, .is_comptime = false, .alignment = alignment };
         i += 1;
     }
 
-    const typeInfo = std.builtin.TypeInfo{
-        .Struct = .{ 
-            .layout = std.builtin.TypeInfo.ContainerLayout.Packed,
-            .fields = &fields,
-            .decls = &[_]std.builtin.TypeInfo.Declaration{},
-            .is_tuple = false,
-        }
-    };
+    const typeInfo = std.builtin.TypeInfo{ .Struct = .{
+        .layout = std.builtin.TypeInfo.ContainerLayout.Packed,
+        .fields = &fields,
+        .decls = &[_]std.builtin.TypeInfo.Declaration{},
+        .is_tuple = false,
+    } };
 
     return @Type(typeInfo);
 }
@@ -297,7 +295,7 @@ fn vlc_entry_setup(vlc_set: vlc_set_cb, vlc_plugin: *c_void) !void {
         config: *c.module_config_t = undefined,
 
         fn init(self: Self) !void {
-            return self.setOnPlugin(c.VLC_MODULE_CREATE, .{ &self.module });
+            return self.setOnPlugin(c.VLC_MODULE_CREATE, .{&self.module});
         }
 
         fn setOnPlugin(self: Self, comptime k: c_int, arg: anytype) !void {
@@ -318,10 +316,10 @@ fn vlc_entry_setup(vlc_set: vlc_set_cb, vlc_plugin: *c_void) !void {
 
         fn set(self: Self, comptime k: c_int, v: anytype, arg: anytype) !void {
             // until zig implements c vararg interop
-            const rv = switch(arg.len) {
+            const rv = switch (arg.len) {
                 1 => self.vlc_set(self.vlc_plugin, v, k, arg[0]),
                 2 => self.vlc_set(self.vlc_plugin, v, k, arg[0], arg[1]),
-                else => @compileError("unknown arg arity")
+                else => @compileError("unknown arg arity"),
             };
 
             if (rv != 0) {
@@ -331,16 +329,16 @@ fn vlc_entry_setup(vlc_set: vlc_set_cb, vlc_plugin: *c_void) !void {
     } = .{ .vlc_plugin = vlc_plugin, .vlc_set = vlc_set };
 
     try m.init();
-    try m.setOnModule(c.VLC_MODULE_NAME, .{ "panner" });
-    try m.setOnModule(c.VLC_MODULE_DESCRIPTION, .{ "audio channel panner / mixer" });
-    try m.setOnModule(c.VLC_MODULE_CAPABILITY, .{ "audio filter" });
-    try m.setOnModule(c.VLC_MODULE_SCORE, .{ @as(c_int, 1) });
+    try m.setOnModule(c.VLC_MODULE_NAME, .{"panner"});
+    try m.setOnModule(c.VLC_MODULE_DESCRIPTION, .{"audio channel panner / mixer"});
+    try m.setOnModule(c.VLC_MODULE_CAPABILITY, .{"audio filter"});
+    try m.setOnModule(c.VLC_MODULE_SCORE, .{@as(c_int, 1)});
 
     try m.addTypeInner(c.CONFIG_CATEGORY);
-    try m.setOnConfig(c.VLC_CONFIG_VALUE, .{ c.CAT_AUDIO });
+    try m.setOnConfig(c.VLC_CONFIG_VALUE, .{c.CAT_AUDIO});
 
     try m.addTypeInner(c.CONFIG_SUBCATEGORY);
-    try m.setOnConfig(c.VLC_CONFIG_VALUE, .{ c.SUBCAT_AUDIO_AFILTER });
+    try m.setOnConfig(c.VLC_CONFIG_VALUE, .{c.SUBCAT_AUDIO_AFILTER});
 
     //var o = @ptrCast(?*c_void, OpenFilter);
 
